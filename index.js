24958c955718e4ab8f9effec2404b84875ed7273
MillieTaffa <161293050+MillieTaffa@users.noreply.github.com>
2024-11-15 15:22:54 +0200

    updated js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function generateNames(event) {
     let userInput = document.querySelector('.search').value;
     let apiKey = "a1f90bc0bf23da8c35fe325tob5f8845";
     let prompt = `Generate a list of baby names ${userInput}.`;
-    let context = "You're an expert in every languages, with each prompt, you're going to give a list of baby names with their meaning and what language the names are in the following formate Name(pronunciantion,language):meaning.Give 20 names"
+    let context = "You're an expert in every languages, with each prompt, you're going to give a list of baby names with their meaning and what language the names are in the following formate Name(pronunciantion,language):meaning.Give 30 names in alphabetical order."
 
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
